582c5f8e5457312f2439042b929fd841af4e50ed
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-05-08 19:30:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,11 +12,19 @@ int add_numbers(int &x, int y){
 int main(){
 	int num1=5;
 	int num2=300;
-	int addr = &num2;
-	int *ptr = *num2;
+	int *ptr = &num2;
 	
 	cout << "Value of num2 is: " << num2 << endl;
 	cout << "Value of ptr is: " << ptr << endl;
-	
+	num2 = 9999;
+	cout << "Value of num2 is: " << num2 << endl;
+	cout << "Value of ptr is: " << ptr << endl;
+	ptr = &num1;;
+	cout << "Value of num2 is: " << num2 << endl;
+	cout << "Value of ptr is: " << ptr << endl;
+	num2 = 9999;
+	cout << "Value of num2 is: " << num2 << endl;
+	cout << "Value of ptr is: " << ptr << endl;
+	num2 = 9999;
 	return 0;
 }
