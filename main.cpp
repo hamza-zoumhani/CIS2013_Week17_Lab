f1eb0be266f0af54be2707c7084bf5bc0a96c931
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-05-08 19:08:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 #define DEBUG false
 
+int add_numbers(int &x, int y){
+		return x+=y;
+}
+
 int main(){
-	
+	/* 
 	bool TRUE=true;
 	
 	if(DEBUG){ cout << "Value of TRUE: " << TRUE << endl; }
@@ -16,7 +20,14 @@ int main(){
 	} else{
 		cout << "...and less to the screen." << endl;
 	}
+	 */
 	
+	int num1=5;
+	int num2=300;
+	
+	cout << "Adding 5 to 500: " << add_numbers(num1,num2) << endl;
+	cout << "Adding 5 to 500: " << add_numbers(num1,num2) << endl;
+	cout << "Adding 5 to 500: " << add_numbers(num1,num2) << endl;
 	
 	return 0;
 }
